dbb7604cda494e31db65fd113040c9a88a1c9e4f
Abanoub <abanoubshowky2018@gmail.com>
2020-05-27 20:35:12 -0700

    add logic

--- app/src/main/java/com/example/tiktaktok/MainActivity.java
+++ app/src/main/java/com/example/tiktaktok/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.tiktaktok;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
@@ -13,46 +15,31 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button playAgainButton;
-    TextView resultTextView;
     GridLayout gridLayout;
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-
-
-        playAgainButton = (Button) findViewById(R.id.playAgainButton);
-        resultTextView = (TextView) findViewById(R.id.winnerTextView);
-        gridLayout = findViewById(R.id.gridLayout);
-        playAgainButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                player = 0;
-                gameActive = true;
-                resultTextView.setVisibility(View.INVISIBLE);
-                playAgainButton.setVisibility(View.INVISIBLE);
-                for(int i=0; i<gridLayout.getChildCount(); i++) {
-                    ImageView image = (ImageView) gridLayout.getChildAt(i);
-                    image.setImageDrawable(null);
-//                  use this loop to reset gameState Array also
-                    gameStates[i] =2;
-                }
-            }
-        });
+        gridLayout = (GridLayout) findViewById(R.id.gridLayout);
     }
 
     //    player = 0 (player red)  player = 1 (player yellow)  2 means empty image view
-    int player = 0;
+    static int player = 0;
     int[] gameStates = {2, 2, 2, 2, 2, 2, 2, 2, 2};
     int[][] winingPosition = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
     boolean gameActive = true;
+    int counter=0;
 
     public void dropIn(View view) {
         ImageView imageView = (ImageView) view;
         int tag = Integer.parseInt(view.getTag().toString());
-        Toast.makeText(this, gameStates[tag]+"", Toast.LENGTH_SHORT).show();
+
         if (gameStates[tag] == 2 && gameActive) {
             imageView.setTranslationY(-1500);
             gameStates[tag] = player;
@@ -68,23 +55,61 @@ public class MainActivity extends AppCompatActivity {
                 if (gameStates[positions[0]] == gameStates[positions[1]] && gameStates[positions[1]] == gameStates[positions[2]] && gameStates[positions[0]] != 2) {
                     if (gameStates[positions[0]] == 0) {
 //                    owning player 0 (red)
-                        Toast.makeText(this, "player red !", Toast.LENGTH_SHORT).show();
-                        resultTextView.setText("player red");
-                        resultTextView.setVisibility(View.VISIBLE);
-                        playAgainButton.setVisibility(View.VISIBLE);
                         gameActive = false;
+                        showAlertDialog("red",R.drawable.red);
                     } else {
 //                      owning player 1 (yellow)
-                        resultTextView.setText("player yellow");
-                        resultTextView.setVisibility(View.VISIBLE);
-                        playAgainButton.setVisibility(View.VISIBLE);
                         gameActive = false;
+                        showAlertDialog("yellow",R.drawable.yellow);
                     }
                 }
             }
-
+            for (int i=0;i<gameStates.length;i++){
+                if(gameStates[i]!=2){
+                    counter++;
+                }
+            }
+            if(counter==gameStates.length){
+                gameActive=false;
+                showAlertDialog("draw",R.drawable.image_draw);
+            }else {
+                counter=0;
+            }
             imageView.animate().translationYBy(1500).rotation(5000).setDuration(200);
         }
     }
 
+
+    public void showAlertDialog(String player, int imageDialog) {
+        // custom dialog
+        final Dialog dialog = new Dialog(context);
+        dialog.setContentView(R.layout.cutom_dialog);
+
+        TextView text = (TextView) dialog.findViewById(R.id.text);
+        if(!player.equals("draw")){
+            text.setText(player+" win !");
+        }else {
+            text.setText("it's a draw");
+        }
+        ImageView image = (ImageView) dialog.findViewById(R.id.image);
+        image.setImageResource(imageDialog);
+
+        Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+        // if button is clicked, close the custom dialog
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.player = 0;
+                gameActive = true;
+                for (int i = 0; i < gridLayout.getChildCount(); i++) {
+                    ImageView image = (ImageView) gridLayout.getChildAt(i);
+                    image.setImageDrawable(null);
+//                  use this loop to reset gameState Array also
+                    gameStates[i] = 2;
+                }
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
 }
